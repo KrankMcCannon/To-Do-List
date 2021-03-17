6f41017a073602d5e27a557624e3d8504656feed
KrankMcCannon <edoardo.valentini@live.it>
2021-03-17 19:18:06 +0100

    if refactored

--- index.js
+++ index.js
@@ -3,27 +3,29 @@ const btnAdd = document.getElementById('buttonAdd');
 btnAdd.addEventListener('click', function(e) {
     const inputAdd = document.getElementById('whatodo');
     
-    if(inputAdd.value) {
-        let value = inputAdd.value;
-        const li = document.createElement('li');
-        const text = document.createTextNode(value);
-        const check = document.createElement('input');
-        check.type = 'checkbox';
+    if(!(inputAdd.value)){
+        return;
+    } 
+    
+    let value = inputAdd.value;
+    const li = document.createElement('li');
+    const text = document.createTextNode(value);
+    const check = document.createElement('input');
+    check.type = 'checkbox';
 
-        li.appendChild(check);
-        li.appendChild(text);
+    li.appendChild(check);
+    li.appendChild(text);
 
-        const ul = document.getElementById('todolist');
-        ul.appendChild(li);
+    const ul = document.getElementById('todolist');
+    ul.appendChild(li);
 
-        inputAdd.value = '';
+    inputAdd.value = '';
 
-        check.addEventListener('change', function(e) {
-            if(e.target.checked) {
-                li.className = 'barrato';
-            } else {
-                li.className = '';
-            }
-        })
-    }
-})
+    check.addEventListener('change', function(e) {
+        if(e.target.checked) {
+            li.className = 'barrato';
+        } else {
+            li.className = '';
+        }
+    });
+});
