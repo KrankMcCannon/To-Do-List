af24486de4bc2861acd9ee90ccb036aa18b2aeb0
KrankMcCannon <edoardo.valentini@live.it>
2021-03-18 14:19:51 +0100

    update functionalities

--- index.js
+++ index.js
@@ -1,31 +1,57 @@
 const btnAdd = document.getElementById('buttonAdd');
+const btnCategory = document.getElementById('dropBtn');
+const dropList = document.getElementById('dropDownList');
+const inputAdd = document.getElementById('whatodo');
+
+const table1 = document.getElementById('todolist1');
+const table2 = document.getElementById('todolist2');
+const table3 = document.getElementById('todolist3');
+
+function dropDown() {
+    dropList.classList.toggle('show');
+}
+
+window.onclick = function(e) {
+    if(!e.target.matches('.dropBtn')) {
+        const dropdowns = document.getElementsByClassName('dropdown-content');
+        for(let i = 0; i < dropdowns.length; i++) {
+            let openDropdown = dropdowns[i];
+            if(openDropdown.classList.contains('show')) {
+                openDropdown.classList.remove('show');
+            }
+        }
+    }
+}
 
 btnAdd.addEventListener('click', function(e) {
-    const inputAdd = document.getElementById('whatodo');
     
     if(!(inputAdd.value)){
         return;
     } 
     
     let value = inputAdd.value;
-    const li = document.createElement('li');
+    const tr = document.createElement('tr');
+    const tdcheck = document.createElement('td');
+    const tdname = document.createElement('td');
+    tr.appendChild(tdcheck);
+    tr.appendChild(tdname);
     const text = document.createTextNode(value);
     const check = document.createElement('input');
     check.type = 'checkbox';
+    tdcheck.className = 'checkbox';
 
-    li.appendChild(check);
-    li.appendChild(text);
+    tdcheck.appendChild(check);
+    tdname.appendChild(text);
 
-    const ul = document.getElementById('todolist');
-    ul.appendChild(li);
+    table1.appendChild(tr);
 
     inputAdd.value = '';
 
     check.addEventListener('change', function(e) {
         if(e.target.checked) {
-            li.className = 'barrato';
+            tdname.className = 'barrato';
         } else {
-            li.className = '';
+            tdname.className = '';
         }
     });
 });
